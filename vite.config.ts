83bbfb406d4ce4a0dee3a790080ef8382cf206b4
Dmitry <machete75111@gmail.com>
2025-02-25 19:01:42 +0300

    test v.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/store/",
+    base: "/store/", // Замените "store" на название вашего репозитория!
+    build: {
+        outDir: "dist", // Папка для GitHub Pages
+    },
 });
