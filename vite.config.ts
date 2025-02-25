0c948c30bddc4d1901615cb325c18cc76af8745c
Dmitry <machete75111@gmail.com>
2025-02-25 20:41:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/store/", // Название репозитория
+    base: "./", // Название репозитория
     build: {
         outDir: "dist", // Куда собирать проект
         emptyOutDir: true, // Удаляет старые файлы перед билдом
