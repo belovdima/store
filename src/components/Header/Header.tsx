08b91f3262a45e0ac07988511b7e3ae8eee7a31e
Dmitry <machete75111@gmail.com>
2025-02-25 17:39:36 +0300

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="header">
-            <nav className="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/shop">Shop</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-            </nav>
+            <div className="container navbar">
+                {/* Логотип */}
+                <Link to="/" className="logo">
+                    STORE
+                </Link>
+
+                {/* Меню */}
+                <nav className={`nav-links ${isOpen ? "open" : ""}`}>
+                    <Link to="/" onClick={() => setIsOpen(false)}>
+                        Home
+                    </Link>
+                    <Link to="/shop" onClick={() => setIsOpen(false)}>
+                        Shop
+                    </Link>
+                    <Link to="/about" onClick={() => setIsOpen(false)}>
+                        About
+                    </Link>
+                    <Link to="/contact" onClick={() => setIsOpen(false)}>
+                        Contact
+                    </Link>
+                </nav>
+
+                {/* Кнопка-бургер для мобилок */}
+                <button className="burger" onClick={() => setIsOpen(!isOpen)}>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </button>
+            </div>
         </header>
     );
 };
